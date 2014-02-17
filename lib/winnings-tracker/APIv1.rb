e8591d09ff10a06fc0b96f9e1b9a480ac30625d5
Steven Lotito <steven.lotito@alumni.pitt.edu>
2014-02-16 21:05:48 -0800

    Cleaned up app and tests, added TrackedVisit

--- lib/winnings-tracker/APIv1.rb
+++ lib/winnings-tracker/APIv1.rb
@@ -1,25 +1,37 @@
+require 'grape'
+require 'dm-core'
+require 'dm-migrations'
+require 'dm-serializer'
+
+DataMapper::Logger.new($stdout, :debug)
+logger = Logger.new($stdout)
+
+uri = "sqlite3://#{Dir.pwd}/winnings.db"
+
+DataMapper.setup(:default, uri)
+
+class Location
+  include DataMapper::Resource
+  property :id,    Serial
+  property :name,  String, unique: true
+  has n, :tracked_visit
+end
+
+class TrackedVisit
+  include DataMapper::Resource
+  property :id, Serial
+  property :visit_date, DateTime, required: true, default: ->(p,s) { DateTime.now }
+  property :buy_in, Decimal, default: 0.00
+  property :cash_out, Decimal, default: 0.00
+  belongs_to :location
+end
+
+DataMapper.finalize
+DataMapper.auto_migrate!
+
 module WinningsTracker
   class APIv1 < Grape::API
-    require 'dm-core'
-    require 'dm-migrations'
-    require 'dm-serializer'
-  
-    DataMapper::Logger.new($stdout, :debug)
-    logger = Logger.new($stdout)
 
-    uri = "sqlite3://#{Dir.pwd}/winnings.db"
-
-    DataMapper.setup(:default, uri)
-
-    class Location
-      include DataMapper::Resource
-      property :id,    Serial
-      property :name,  String, unique: true
-    end
-
-    DataMapper.finalize
-    DataMapper.auto_upgrade!
-    
     version 'v1', using: :path
     format :json
     default_format :json
--- spec/apiv1_spec.rb
+++ spec/apiv1_spec.rb
@@ -7,8 +7,14 @@ describe  WinningsTracker::APIv1 do
     WinningsTracker::APIv1
   end
 
-  it "should run" do
+  it "should serve up a 404 for a link that doesn't exist" do
     get '/locations'
-    last_response.should eql "wtf"
+    last_response.status.should eql 404
+  end
+
+  it "should run" do
+    get '/v1/locations'
+    last_response.status.should eql 200
+    last_response.headers["Content-type"].should eql "application/json"
   end
 end
